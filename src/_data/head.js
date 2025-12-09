4fde607ea77ae3cdcb71de3f7f6b06a62b3bc90b
Cristovao Verstraeten <cristovao@apleasantview.com>
2025-12-09 12:09:52 +0100

    Make some proper use of the head generator

--- src/_data/head.js
+++ src/_data/head.js
@@ -1,15 +1,8 @@
 export default {
-	meta: [
-		{ name: "description", content: "Default site description placeholder." },
-		{ name: "twitter:description", content: "Default Twitter description." }
-	],
-	description: "Default site description.",
-	twitter: {
-		"twitter:description": "Default Twitter description.",
-		"twitter:card": "summary_large_image",
-		"random": "This key is ignored by flattening."
-	},
 	link: [
 		{ rel: "stylesheet", href: "/assets/css/index.css" }
+	],
+	script: [
+		{ src: "/assets/js/index.js", defer: true}
 	]
 }
